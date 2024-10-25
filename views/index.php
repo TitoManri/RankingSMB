bae8f0f845419a8bfee52b4fcd7af7e236eb64e9
Tito <manri.carazo@gmail.com>
2024-10-24 22:31:18 -0600

    MongoDB a PHP
    Commit de Conexion de Mongo a PHP

--- views/index.php
+++ views/index.php
@@ -1,3 +1,20 @@
 <?php
-phpinfo();
-?>
+require '../vendor/autoload.php'; // Asegúrate de tener Composer instalado y MongoDB cliente instalado
+
+try {
+    // Crear una conexión a MongoDB
+    $client = new MongoDB\Client("mongodb://localhost:27017");
+    echo "Conexión a MongoDB establecida con éxito.<br>";
+
+    // Seleccionar la base de datos
+    $database = $client->selectDatabase('HeladosMemphis');
+
+    // Verificar las colecciones
+    $collections = $database->listCollections();
+    echo "Colecciones en 'HeladosMenphis':<br>";
+    foreach ($collections as $collection) {
+        echo $collection->getName() . "<br>";
+    }
+} catch (Exception $e) {
+    echo "Error: " . $e->getMessage();
+}
