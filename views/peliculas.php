6401e3035ce0a8e7656a8d4f3862fa863f75550f
Maripaz <144374635+Maripaz14@users.noreply.github.com>
2024-10-25 19:57:15 -0600

    películas
    filtros (falta modficar por problemas de boostrap y bulma)

--- views/peliculas.php
+++ views/peliculas.php
@@ -9,62 +9,73 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css">
+    <!--<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css">-->
     <link rel="stylesheet" href="./assets/css/header.css">
-    <link rel="stylesheet" href="./assets/css/searchBar.css">
+    <link rel="stylesheet" href="./assets/css/peliculas.css">
 </head>
 
 <body>
     <?php include_once "./templates/Header_Footer/header.php" ?>
 
-    <div class="container mt-5">
-        <!-- dropdown -->
-        <div class="dropdown">
-            <button class="btn btn-secondary dropdown-toggle m-1" type="button" data-bs-toggle="dropdown"
-                aria-expanded="false">
-                Año
-            </button>
-            <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">2023</a></li>
-                <li><a class="dropdown-item" href="#">2022</a></li>
-                <li><a class="dropdown-item" href="#">2021</a></li>
-            </ul>
-        </div>
-        <div class="dropdown">
-            <button class="btn btn-secondary dropdown-toggle m-1" type="button" data-bs-toggle="dropdown"
-                aria-expanded="false">
-                Género
-            </button>
-            <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Comedia</a></li>
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Aventura</a></li>
-            </ul>
-        </div>
-        <div class="dropdown">
-            <button class="btn btn-secondary dropdown-toggle m-1" type="button" data-bs-toggle="dropdown"
-                aria-expanded="false">
-                Rating
-            </button>
-            <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">5</a></li>
-                <li><a class="dropdown-item" href="#">4</a></li>
-                <li><a class="dropdown-item" href="#">3</a></li>
-            </ul>
-        </div>
-
-        <!-- search bar-->
-        <div class="main-search-input-wrap">
-            <div class="main-search-input fl-wrap">
-                <div class="main-search-input-item">
-                    <input type="text" value="" placeholder="Buscar películas">
-                </div>
-                <button class="main-search-button">Buscar</button>
+    <!-- Filtros y búsqueda-->
+    <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <h4 class="mx-5 my-2 text-white">Filtros</h5>
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <!-- Género filtro -->
+                    <li class="nav-item dropdown m-2">
+                        <div class="dropdown">
+                            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown"
+                                aria-expanded="false">
+                                Género
+                            </button>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Acción</a></li>
+                                <li><a class="dropdown-item" href="#">Comedia</a></li>
+                                <li><a class="dropdown-item" href="#">Terror</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <!-- Calificación -->
+                    <li class="nav-item dropdown m-2">
+                        <div class="dropdown">
+                            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown"
+                                aria-expanded="false">
+                                Calificación
+                            </button>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">5</a></li>
+                                <li><a class="dropdown-item" href="#">4</a></li>
+                                <li><a class="dropdown-item" href="#">3</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <!-- Año filtro -->
+                    <li class="nav-item dropdown m-2">
+                        <div class="dropdown">
+                            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown"
+                                aria-expanded="false">
+                                Año
+                            </button>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">2024</a></li>
+                                <li><a class="dropdown-item" href="#">2023</a></li>
+                                <li><a class="dropdown-item" href="#">2022</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Descubre contenido" aria-label="Buscar">
+                    <button class="btn" id="boton-busqueda" type="submit">Buscar</button>
+                </form>
             </div>
         </div>
-    </div>
+    </nav>
 
-    <!-- #region -->
+    <!-- #PARTE DE CATÁLOGO -->
+    
 
     <div class="fixed-bottom">
         <?php include_once "./templates/Header_Footer/footer.php" ?>
