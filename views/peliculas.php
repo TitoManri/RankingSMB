84adb148f2d56dff2dc4f815da7742e213fbddf0
Maripaz <mtenoriobaldi@gmail.com>
2024-11-10 23:03:44 -0600

    Cambios en select y estilos
    S hace uso de select2 para los filtros y se agrega el env al git ignore

--- views/peliculas.php
+++ views/peliculas.php
@@ -5,78 +5,81 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Películas</title>
-    <!-- Links -->
+    <!-- LINKS -->
+    <!-- select2-->
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <!-- boostrap-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- bulma-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+    <!-- boostrap icons-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <!-- material icons-->
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <!-- css personalizado-->
     <link rel="stylesheet" href="./assets/css/catalogo.css">
     <link rel="stylesheet" href="./assets/css/cards.css">
     <link rel="stylesheet" href="./assets/css/header.css">
-    <link rel="stylesheet" href="./assets/css/peli.css">
+    <link rel="stylesheet" href="./assets/css/pelicula.css">
 </head>
 
 <body>
     <!-- HEADER-->
     <?php include_once "./templates/Header_Footer/header.php" ?>
 
-    <!-- Filtros y búsqueda-->
+    <!-- Barra de Filtros y Búsqueda -->
     <nav class="navbar navbar-expand-lg mb-5" id="filtros">
         <div class="container-fluid">
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <h4 class="mx-5 my-2 title is-4 has-text-white">Filtros</h4>
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <!-- Género filtro -->
-                    <li class="nav-item dropdown m-2">
-                        <div class="dropdown">
-                            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown"
-                                aria-expanded="false">
-                                Género
-                            </button>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Acción</a></li>
-                                <li><a class="dropdown-item" href="#">Comedia</a></li>
-                                <li><a class="dropdown-item" href="#">Terror</a></li>
-                            </ul>
+                <div class="row w-100">
+                    <!-- Filtros -->
+                    <div class="col-md-6">
+                        <h4 class="mx-5 my-2 title is-4 has-text-white">Filtros</h4>
+                        <div class="row ml-2">
+                            <div class="col-md-4 my-1">
+                                <!-- Filtro género -->
+                                <select id="genero" class="form-select m-3" aria-label="Default select example">
+                                    <option selected>Género</option>
+                                    <option value="Terror">Terror</option>
+                                    <option value="Comedia">Comedia</option>
+                                    <option value="Acción">Acción</option>
+                                </select>
+                            </div>
+                            <div class="col-md-4 my-1">
+                                <!-- Filtro calificación -->
+                                <select id="calificacion" class="form-select m-3" aria-label="Default select example">
+                                    <option selected>Calificación</option>
+                                    <option value="5">5</option>
+                                    <option value="4">4</option>
+                                    <option value="3">3</option>
+                                </select>
+                            </div>
+                            <div class="col-md-4 my-1">
+                                <!-- Filtro año -->
+                                <select id="año" class="form-select m-3" aria-label="Default select example">
+                                    <option selected>Año</option>
+                                    <option value="2024">2024</option>
+                                    <option value="2015">2015</option>
+                                    <option value="1990">1990</option>
+                                </select>
+                            </div>
                         </div>
-                    </li>
-                    <!-- Calificación -->
-                    <li class="nav-item dropdown m-2">
-                        <div class="dropdown">
-                            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown"
-                                aria-expanded="false">
-                                Calificación
-                            </button>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">5</a></li>
-                                <li><a class="dropdown-item" href="#">4</a></li>
-                                <li><a class="dropdown-item" href="#">3</a></li>
-                            </ul>
-                        </div>
-                    </li>
-                    <!-- Año filtro -->
-                    <li class="nav-item dropdown m-2">
-                        <div class="dropdown">
-                            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown"
-                                aria-expanded="false">
-                                Año
-                            </button>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">2024</a></li>
-                                <li><a class="dropdown-item" href="#">2023</a></li>
-                                <li><a class="dropdown-item" href="#">2022</a></li>
-                            </ul>
-                        </div>
-                    </li>
-                </ul>
-                <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Descubre contenido" aria-label="Buscar">
-                    <button class="btn" id="boton-busqueda" type="submit">Buscar</button>
-                </form>
+                    </div>
+                    <!-- Campo de búsqueda -->
+                    <div class="col-md-2"></div>
+                    <div class="col-md-4 mt-5 pt-5">
+                        <form class="d-flex" role="search">
+                            <input class="form-control me-2 mb-3" type="search" placeholder="Descubre contenido"
+                                aria-label="Buscar">
+                            <button class="btn btn-primary" id="boton-busqueda" type="submit">Buscar</button>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
     </nav>
+
 
     <!-- #PARTE DE CATÁLOGO -->
     <!-- Parte de netflix -->
@@ -297,12 +300,14 @@
         <?php include_once "./templates/Header_Footer/footer.php" ?>
     </div>
 </body>
-<!--Scripts-->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-    crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!-- SCRIPTS -->
+<!-- jQuery -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<!-- select2 -->
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+<!-- filtros películas -->
+<script src="./assets/js/peliculasFiltros.js"></script>
+<!-- boostrap-->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
     crossorigin="anonymous"></script>
