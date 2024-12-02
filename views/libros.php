30b086745d58788c48ccdb786a37ccac001ddf4e
Maripaz <mtenoriobaldi@gmail.com>
2024-12-02 16:41:33 -0600

    libros en bd
    faltan los de buscar

--- views/libros.php
+++ views/libros.php
@@ -153,6 +153,6 @@ if (!empty($_SESSION['correo'])) {
     integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
     crossorigin="anonymous"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-<script src="./assets/js/librosP.js"></script>
+<script src="./assets/js/librosA.js"></script>
 
 </html>
